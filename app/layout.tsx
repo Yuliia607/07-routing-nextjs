bf4d27eb6933515dcf86efe44728a19861e8065b
Yuliia Dyrda <yuliia0704@gmail.com>
2026-06-30 19:37:39 +0200

    Add note preview modal with intercepting routes

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,8 +24,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  modal,
 }: Readonly<{
   children: React.ReactNode;
+  modal: React.ReactNode;
 }>) {
   return (
     <html
@@ -37,7 +39,7 @@ export default function RootLayout({
           <Header />
 
           {children}
-
+ {modal}
           <Footer />
         </TanStackProvider>
       </body>
